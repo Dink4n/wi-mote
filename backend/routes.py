360da6058156f44e15afdc95e3d8c7ec3ed82a59
JimboKearn <anujithbahuleyan@gmail.com>
2020-10-08 16:02:22 +0530

    fixed index

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,21 @@
 from backend import app
 from backend.controller import handle_keypress
-from flask import request
+from flask import request, render_template
 
 
-@app.route('/keypress')
+@app.route("/")
+def index():
+    '''
+    Serves the index page on /
+    '''
+    return render_template("index.html")
+
+
+@app.route("/keypress")
 def keypress():
+    '''
+    Listens on /keypress for key presses.
+    '''
     key = request.args.get("key")
     handle_keypress(key)
 
