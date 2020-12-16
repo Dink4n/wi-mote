edf465f701176e32947ad44361b5c15714df4659
JimboKearn <anujithbahuleyan@gmail.com>
2020-12-16 09:35:20 +0530

    replace power button with fullscreen button

--- volume-changer/src/components/Utilities.js
+++ volume-changer/src/components/Utilities.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import { FaVolumeMute } from "react-icons/fa";
+import { FaExpandArrowsAlt } from "react-icons/fa";
 import { AwesomeButton } from "react-awesome-button";
-import { MdPowerSettingsNew } from "react-icons/md";
 import { handleKeyPress } from "../api";
 
 export const Utilities = () => {
@@ -20,9 +20,9 @@ export const Utilities = () => {
 				<Grid.Column>
 					<AwesomeButton
 						type="primary"
-						// onPress={() => handleKeyPress("poweroff")}
+						onPress={() => handleKeyPress("fullscreen")}
 					>
-						<MdPowerSettingsNew size={20} />
+						<FaExpandArrowsAlt size={20} />
 					</AwesomeButton>
 				</Grid.Column>
 			</Grid.Row>
